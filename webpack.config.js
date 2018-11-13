2b797baffd49f30d05e50c534a287890e3053469
Nick Owen <owennw@users.noreply.github.com>
2018-11-13 17:03:08 +0000

    build(webpack): Use defaults from webpack 4
    Changes location of built assets from lib/ to dist/, and minifies the output

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.ts',
   devtool: 'inline-source-map',
-  mode: 'development',
-  output: {
-    filename: 'index.js',
-    path: path.resolve('lib'),
-  },
+  mode: 'production',
   module: {
     rules: [{
       test: /\.(ts|js)$/,
